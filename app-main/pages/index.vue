0c3b6febea260847b8b2ab3db9f0d12f8548c6c2
Andrew Bui <andrew@vmodev.com>
2022-07-03 23:14:34 +0700

    init project

---
+++ app-main/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import {
+  registerMicroApps,
+  start,
+  setDefaultMountApp,
+} from 'qiankun'
+export default {
+  mounted() {
+    this.init()
+  },
+  computed: {
+    ...mapState(['apps'])
+  },
+  methods: {
+    async init() {
+      registerMicroApps(this.apps)
+      setDefaultMountApp(this.apps[0].activeRule)
+      start()
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+
+#subapp {
+  min-height: 100vh;
+}
+</style>
---
+++ app-main/store/index.js
@@ -0,0 +1,75 @@
+import {
+  initGlobalState,
+} from 'qiankun';
+
+const TYPES = {
+  INIT_APPS: 'init_apps'
+}
+
+export const state = () => ({
+  apps: [],
+  token: null,
+  sdk: null
+})
+
+export const mutations = {
+  [TYPES.INIT_APPS](state, apps) {
+    const actions = initGlobalState({
+      token: state.token,
+    })
+    const sdk = {
+      globalState: actions,
+      go2404: () => {
+        this.$router.push('404')
+      },
+      token: state.token
+    }
+    apps = apps.map((item) => ({
+      ...item,
+      container: '#sub-app',
+      props: {
+        sdk,
+      },
+    }))
+    const routes = apps.map((item, i) => ({
+      path: `${item.activeRule}(.*)`,
+      name: `${item.name}-${i}`,
+      component: () => import('@/pages/index.vue').then(m => m.default || m)
+    }))
+    this.$router.addRoutes([].concat(...routes,
+      {
+        path: `*`,
+        name: `404`,
+        component: () => import('@/pages/404.vue').then(m => m.default || m)
+      }
+    ))
+
+    state.apps = apps
+    state.sdk = sdk
+  }
+}
+
+export const actions = {
+  async getMenus({commit}) {
+    const {payload} = await getMenus()
+
+    commit(TYPES.INIT_APPS, payload)
+  }
+}
+
+
+async function getMenus() {
+
+  return {
+    code: 0,
+    payload: [
+      {
+        name: 'Campaign',
+        activeRule: '/campaign',
+        entry: 'http://127.0.0.1:7102/',
+      },
+    ],
+    message: 'success',
+  }
+
+}
